b9bfe7b93afb8b2e86681e6c0b8efb068ecb8986
copiesofcopies <aaron@copiesofcopies.org>
2013-01-08 17:59:39 -0500

    __main__ now cycles through pages of repos and looks for potential license files in each. Added config.yaml for impending addition of github auth.

--- ghretrieve.py
+++ ghretrieve.py
@@ -2,6 +2,9 @@ import requests
 import json
 from urlparse import urljoin
 from base64 import b64decode
+import optparse
+import time
+import link_header
 
 def get_repo(repo_url):
     r = requests.get(repo_url)
@@ -13,13 +16,11 @@ def get_repo(repo_url):
     return None
 
 def get_repo_license(repo_url):
-    license_files = {'COPYING':None, 'LICENSE':None}
+    license_files = {'COPYING':None, 'LICENSE':None, 'README':None}
 
     copying_url = "%s/contents/%s" % (repo_url, 'COPYING')
     license_url = "%s/contents/%s" % (repo_url, 'LICENSE')
-
-    print "LICENSE url: %s" % license_url
-    print "COPYING url: %s" % copying_url
+    readme_url = "%s/readme" % repo_url
 
     r = requests.get(copying_url)
     if(r.ok):
@@ -31,19 +32,74 @@ def get_repo_license(repo_url):
         license_file = json.loads(r.text or r.content)
         license_files['LICENSE'] = license_file
 
+    r = requests.get(readme_url)
+    if(r.ok):
+        readme_file = json.loads(r.text or r.content)
+        license_files['README'] = readme_file
+
     return license_files
 
 if __name__ == "__main__":
-    test_url = "https://api.github.com/repos/copiesofcopies/github-license-analysis"
 
-    repo_item = get_repo(test_url)
-    licenses = get_repo_license(test_url)
+    # Set up the command line argument parser
+    parser = optparse.OptionParser()
 
-    if (licenses['COPYING']):
-        print 'Repository "%s" has a COPYING file: %s' % (repo_item['full_name'], 
-                                                                   b64decode(licenses['COPYING']['content']))
-    elif (licenses['LICENSE']):
-        print 'Repository "%s" has a LICENSE file: %s' % (repo_item['full_name'], 
-                                                                   b64decode(licenses['LICENSE']['content']))
-    else:
-        print 'Repository "%s" has no top-level COPYING or LICENSE file' % repo_item['full_name']
+    parser.add_option('-u', '--url',
+                      action="store", dest="url",
+                      help="""URL to begin retriving repositories with
+                              ('next' link from last result)""",
+                      default="")
+    
+    options, args = parser.parse_args()
+
+    # Get the URL from the 'url' argument or start from square one
+    repos_url = options.url or "https://api.github.com/repositories"
+    next_repos_url = repos_url
+
+    # Retrieve pages of repos till rate limit is reached
+    requests_left = 5000
+    r = requests.get(next_repos_url)
+
+    while(r.ok):
+        repos_json = json.loads(r.text or r.content)
+        requests_left = repos_json['X-RateLimit-Remaining']
+
+        # Get link for next page of repos
+        links = link_header.parse_link_value(r.headers['link'])
+
+        for link_url in links:
+            if(links[link_url]['rel'] == 'next'):
+                next_repos_url = link_url
+
+        # Process this page of repos
+        for repo in repos_json:
+            # If we're out of requests, sleep in 5-minute increments
+            while(requests_left < 4):
+                print "Waiting for rate limit to reset..."
+                time.sleep(300)
+                rl_resp = requests.get("https://api.github.com/rate_limit")
+                if(rl_resp.ok):
+                    rl_json = json.loads(rl_resp.text or rl_resp.content)
+                    requests_left = rl_json['X-RateLimit-Remaining']
+
+            licenses = get_repo_license(repo['url'])
+
+            print "Repository: %s\nFork? %s" % (repo['full_name'], repo['fork'])
+
+            if(licenses['LICENSE']):
+                print "LICENSE: %s" % licenses['LICENSE']['path']
+            else:
+                print "LICENSE? No"
+
+            if(licenses['COPYING']):
+                print "COPYING: %s" % licenses['COPYING']['path']
+            else:
+                print "COPYING? No"
+
+            print ""
+        
+        # Get the next page of repos
+        print "Finished with that batch! Getting the next from %s" %\
+            next_repos_url
+
+        r = requests.get(next_repos_url)
