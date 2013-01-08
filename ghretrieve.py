1bc2587cf945f82f4988312e3378877cace14683
copiesofcopies <aaron@copiesofcopies.org>
2013-01-08 18:33:34 -0500

    Enable authenticated API access; fix some syntax errors.

--- ghretrieve.py
+++ ghretrieve.py
@@ -1,13 +1,18 @@
+# TODO: DB storage, logging
+
 import requests
 import json
-from urlparse import urljoin
-from base64 import b64decode
 import optparse
 import time
+import yaml
 import link_header
+from urlparse import urljoin
+from base64 import b64decode
+
+config = None
 
 def get_repo(repo_url):
-    r = requests.get(repo_url)
+    r = api_request(repo_url)
 
     if(r.ok):
         repo_item = json.loads(r.text or r.content)
@@ -22,24 +27,36 @@ def get_repo_license(repo_url):
     license_url = "%s/contents/%s" % (repo_url, 'LICENSE')
     readme_url = "%s/readme" % repo_url
 
-    r = requests.get(copying_url)
+    r = api_request(copying_url)
     if(r.ok):
         copying_file = json.loads(r.text or r.content)
         license_files['COPYING'] = copying_file
     
-    r = requests.get(license_url)
+    r = api_request(license_url)
     if(r.ok):
         license_file = json.loads(r.text or r.content)
         license_files['LICENSE'] = license_file
 
-    r = requests.get(readme_url)
+    r = api_request(readme_url)
     if(r.ok):
         readme_file = json.loads(r.text or r.content)
         license_files['README'] = readme_file
 
     return license_files
 
+def api_request(url):
+    u = config['github_user'] 
+    p = config['github_password']
+
+    if(u and p):
+        return requests.get(url, auth=(u, p))
+    else:
+        return requests.get(url)
+
 if __name__ == "__main__":
+    # Parse the yaml config file
+    config_file = open('config.yaml', 'r')
+    config = yaml.load(config_file.read())
 
     # Set up the command line argument parser
     parser = optparse.OptionParser()
@@ -58,11 +75,11 @@ if __name__ == "__main__":
 
     # Retrieve pages of repos till rate limit is reached
     requests_left = 5000
-    r = requests.get(next_repos_url)
+    r = api_request(next_repos_url)
 
     while(r.ok):
         repos_json = json.loads(r.text or r.content)
-        requests_left = repos_json['X-RateLimit-Remaining']
+        requests_left = r.headers['X-RateLimit-Remaining']
 
         # Get link for next page of repos
         links = link_header.parse_link_value(r.headers['link'])
@@ -77,10 +94,9 @@ if __name__ == "__main__":
             while(requests_left < 4):
                 print "Waiting for rate limit to reset..."
                 time.sleep(300)
-                rl_resp = requests.get("https://api.github.com/rate_limit")
+                rl_resp = api_request("https://api.github.com/rate_limit")
                 if(rl_resp.ok):
-                    rl_json = json.loads(rl_resp.text or rl_resp.content)
-                    requests_left = rl_json['X-RateLimit-Remaining']
+                    requests_left = rl_resp.headers['X-RateLimit-Remaining']
 
             licenses = get_repo_license(repo['url'])
 
@@ -89,12 +105,12 @@ if __name__ == "__main__":
             if(licenses['LICENSE']):
                 print "LICENSE: %s" % licenses['LICENSE']['path']
             else:
-                print "LICENSE? No"
+                print "LICENSE: None"
 
             if(licenses['COPYING']):
                 print "COPYING: %s" % licenses['COPYING']['path']
             else:
-                print "COPYING? No"
+                print "COPYING: None"
 
             print ""
         
@@ -102,4 +118,4 @@ if __name__ == "__main__":
         print "Finished with that batch! Getting the next from %s" %\
             next_repos_url
 
-        r = requests.get(next_repos_url)
+        r = api_request(next_repos_url)
